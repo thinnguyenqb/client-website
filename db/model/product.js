52344cb12b51be6cbe6af07574218bc33efeeb31
EricNguyen99 <vanthin7111999@gmail.com>
2019-11-19 17:47:20 +0700

    fix product

--- db/model/product.js
+++ db/model/product.js
@@ -3,12 +3,12 @@ var mongoose = require('mongoose');
 var productSchema = mongoose.Schema({
     name: { type: String },
     category: { type: String },
-    manufacturer: { type: String },
     imgSrc: { type: String },
     price: { type: String },
     old_price: { type: String },
-    configuration: { type: String },
-    description: { type: String }
+    producer: { type: String },
+    description: { type: String },
+    configuration: { type: String }
 });
 
 module.exports = mongoose.model('product', productSchema, 'product');
