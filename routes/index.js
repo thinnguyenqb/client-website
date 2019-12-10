80e0f3d16d40f26c795cbf5d31687bdb30206ece
Nguyen Manh Linh <nmlinh2799@gmail.com>
2019-12-10 17:07:53 +0700

    Add advance filter and sort feature

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,7 @@ router.get('/', function(req, res, next) {
 		.catch(err => {
 			console.log('Error: ', err);
 			throw err;
-		})
+		});
 });
 
 // Store
@@ -19,36 +19,57 @@ router.get('/store', function(req, res, next) {
 	// Validate query string
 	const category = (typeof req.query.category != 'undefined')?(req.query.category):'';
 	const producer = (typeof req.query.producer != 'undefined')?(req.query.producer):'';
+	const sort = (typeof req.query.sort != 'undefined')?(req.query.sort):'';
 	const count = (typeof req.query.count != 'undefined')?parseInt(req.query.count):12;
 	const page = (typeof req.query.page != 'undefined')?parseInt(req.query.page):1;
 
-	let params = {};
+	// Arguments for find and sort query
+	let findParams = {};
+	let sortParams = {};
+	// URI query string
 	let classifyStr = '';
+	let sortStr = '';
+
 	if (category != '') {
-		params.category = category;
+		findParams.category = category;
 		classifyStr += ('category=' + category + '&');
 	}
 	if (producer != '') {
-		params.producer = producer;
+		findParams.producer = producer;
 		classifyStr += ('producer=' + producer + '&');
 	}
-	console.log(params);
+	if (sort != '') {
+		sortStr += ('sort=' + sort + '&');
+		if (sort == 'name-asc') {
+			sortParams.name = 1;
+		}
+		if (sort == 'name-des') {
+			sortParams.name = -1;
+		}
+	}
 
-	Product.find(params)
+	Product.find(findParams)
 		.then(allProducts => {
-			Product.find(params).limit(count).skip((page - 1) * count).exec()
+			Product.find(findParams).sort(sortParams).limit(count).skip((page - 1) * count)
 				.then(products => {
 					res.render('pages/store', {
 						user: req.user, // User
 						products: products,
 						name: ' ' + category + ' ' + producer,
+						// Query string
 						classifyStr: classifyStr,
+						sortStr: sortStr,
+						// Remain selection
+						category: category,
+						producer: producer,
+						sort: sort,
 						count: count,
 						page: page,
+						// Creating page index
 						countPage: parseInt(allProducts.length / count +
 							((allProducts.length % count == 0) ? 0 : 1)),
 						countAll: allProducts.length,
-						i: 1 // loop index for creating page index
+						i: 1
 					});
 				})
 				.catch(err => {
@@ -62,6 +83,21 @@ router.get('/store', function(req, res, next) {
 		});
 });
 
+// Advance Filter
+router.post('/store', function(req, res) {
+	const category = (typeof req.body.category != 'undefined')?(req.body.category):'';
+	const producer = (typeof req.body.producer != 'undefined')?(req.body.producer):'';
+
+	let queryStr = '';
+	if (category != '') {
+		queryStr += ('category=' + category + '&');
+	}
+	if (producer != '') {
+		queryStr += ('producer=' + producer + '&');
+	}
+	res.redirect('/store?' + queryStr);
+});
+
 // Product Information
 router.get('/product/:id', function(req, res, next) {
 	Product.findOne({ _id: req.params.id })
@@ -71,7 +107,7 @@ router.get('/product/:id', function(req, res, next) {
 		.catch(err => {
 			console.log('Error: ', err);
 			throw err;
-		})
+		});
 });
 
 // // Categories
